5bf3d4718b1ee35892182d9636ccc7885aa886c3
LAM THANH DAT <thanhdat18112k2@gmail.com>
2023-04-24 09:54:07 +0700

    chuyen den b

---
+++ app/src/main/java/baitap/cnttk13/myapplication/batitap.java
@@ -0,0 +1,15 @@
+package baitap.cnttk13.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class batitap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batitap);
+        //bao cao lan 2
+    }
+}
